6258fb20334bd3cd9fb78bb9fc466bd258f1b362
rasulov-emirlan <rasulov.emirlan@gmail.com>
2022-03-17 20:23:22 +0600

    implement-strstr better than 100% of solutions

--- implement-strstr/main.go
+++ implement-strstr/main.go
@@ -3,11 +3,5 @@ package implementstrstr
 import "strings"
 
 func strStr(haystack string, needle string) int {
-	if needle == "" {
-		return 0
-	}
-	if len(needle) > len(haystack) {
-		return -1
-	}
 	return strings.Index(haystack, needle)
 }
